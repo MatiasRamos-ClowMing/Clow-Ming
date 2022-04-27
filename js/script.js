fe848fd3a916812df5219274a1ca601803d3f4da
Matias Daniel Ramos <matiasdanielramos63@gmail.com>
2022-04-27 19:30:22 -0300

    se soluciono error en el index en la seccion master video / imagen y texto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
+   $('#myInput').trigger('focus')
 })
+
+
+$(document).ready(function(){
+   $('.toast').toast('show'); 
+
+   $("#btnToasts").click(function(){
+   $("#toast2").toast('show'); 
+});    
+
+   $("#btnOcultar").click(function(){
+   $("#toast2").toast('hide'); 
+});      
+});
